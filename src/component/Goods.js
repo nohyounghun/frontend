56c36f18f306ae359f878e415ca51c5250e35891
nohyounghun <vansniping@gmail.com>
2025-06-05 13:16:19 +0900

    Update Goods.js

--- src/component/Goods.js
+++ src/component/Goods.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { AlertContext } from '../AlertContext';
+import { AlertContext } from './AlertContext';
 
 function Goods(props) {
   const [data, setData] = useState([]);
